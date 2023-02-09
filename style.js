f25e1cdc3dfc6085a797749212271e9f0ca9fe1f
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-10 01:49:42 +0900

    :recycle: css -> js 리팩토링

--- style.js
+++ style.js
@@ -1,13 +1,29 @@
 const htmlElement = document.querySelector("html");
 const bodyElement = document.querySelector("body");
+
 const navBarElement = document.querySelector(".navBar");
 // const aTagElement = document.getElementsByTagName("a");
 const navBarLeftElement = document.querySelector(".navBar_left");
 const logoElement = document.querySelector(".logo");
 const navBarRightElement = document.querySelector(".navBar_right");
+
 const profileElement = document.querySelector(".profile");
 const profileTitleElement = document.querySelector(".profile-title");
 const profileSubTitleElement = document.querySelector(".profile-subTitle");
+
+const meImgElement = document.querySelector(".meImg");
+const meImgPhotoElement = document.querySelector(".meImg_img");
+
+const contentElement = document.querySelector(".content");
+const contentMainElement = document.querySelector(".content_main");
+
+const addressBlogElement = document.querySelector(".address_blog");
+
+const playlistNowElement = document.querySelector(".playlist_now");
+const playlistElement = document.querySelector(".playlist");
+
+const workTitleElement = document.querySelector(".work_title");
+const workElement = document.querySelector(".work");
 
 htmlElement.style.backgroundColor = "black";
 
@@ -60,3 +76,64 @@ profileSubTitleElement.style.opacity = "50%";
 profileSubTitleElement.style.textAlign = "center";
 profileSubTitleElement.style.letterSpacing = "2px";
 profileSubTitleElement.style.lineHeight = "16px";
+
+meImgElement.style.width = "250px";
+meImgElement.style.height = "250px";
+meImgElement.style.borderRadius = "30%";
+meImgElement.style.overflow = "hidden";
+meImgElement.style.border = "10px solid silver";
+
+meImgPhotoElement.style.width = "100%";
+meImgPhotoElement.style.height = "100%";
+meImgPhotoElement.style.objectFit = "cover";
+
+meImgElement.addEventListener("mouseover", function () {
+  meImgElement.style.cursor = "pointer";
+  meImgElement.style.transform = "scale(1.04)";
+  meImgElement.style.transition = "all 200ms ease-in-out";
+});
+
+meImgElement.addEventListener("mouseout", function () {
+  meImgElement.style.transform = "scale(1.0)";
+});
+
+// contentElement.style.height = "70vh";
+// contentElement.style.display = "hidden";
+// contentElement.style.flexDirection = "column";
+// contentElement.style.justifyContent = "center";
+// contentElement.style.alignItems = "center";
+// contentElement.style.textAlign = "center";
+// contentElement.style.gap = "40px";
+// contentElement.style.fontFamily = "HBIOS-SYS";
+// contentElement.style.fontSize = "30px";
+
+// contentMainElement.style.color = "white";
+// contentMainElement.style.padding = "40px";
+// contentMainElement.style.width = "22ch";
+// contentMainElement.style.animation =
+//   "typing 2s steps(22), blink 0.5s step-end infinite alternate";
+// contentMainElement.style.whiteSpace = "nowrap";
+// contentMainElement.style.overflow = "hidden";
+// contentMainElement.style.fontSize = "1.8em";
+
+// addressBlogElement.style.margin = "10px";
+
+// playlistElement.style.padding = "50px 80px";
+// playlistElement.style.display = "flex";
+// playlistElement.style.flexDirection = "column";
+// playlistElement.style.justifyContent = "center";
+// playlistElement.style.alignItems = "center";
+
+// playlistNowElement.style.color = "white";
+// playlistNowElement.style.fontSize = "30px";
+// playlistNowElement.style.marginBottom = "20px";
+// playlistNowElement.style.fontFamily = "HBIOS-SYS";
+
+// workTitleElement.style.margin = "100px 0 40px 0";
+
+// workElement.style.color = "white";
+// workElement.style.display = "none";
+// workElement.style.flexDirection = "column";
+// workElement.style.alignItems = "center";
+// workElement.style.gap = "40px";
+// workElement.style.fontSize = "40px";
