81d5b186879abc677782da603ee2a03dce633185
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-10 00:42:35 +0900

    :recycle: css -> js 리팩토링

--- style.js
+++ style.js
@@ -1,8 +1,15 @@
 const htmlElement = document.querySelector("html");
 const bodyElement = document.querySelector("body");
+const navBarElement = document.querySelector(".navBar");
 
 htmlElement.style.backgroundColor = "black";
 
 bodyElement.style.margin = "0 auto";
 bodyElement.style.height = "100vh";
 bodyElement.style.fontFamily = "KIMM_Bold";
+
+navBarElement.style.display = "flex";
+navBarElement.style.justifyContent = "space-between";
+navBarElement.style.alignItems = "center";
+navBarElement.style.color = "rgba(106, 106, 106, 0.013)";
+navBarElement.style.padding = "16px 24px";
