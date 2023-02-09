22cb0e9b043048210592bac68eefb9ec56c26336
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-09 23:29:28 +0900

    :recycle: css -> js 리팩토링

--- style.js
+++ style.js
@@ -1,3 +1,8 @@
 const htmlElement = document.querySelector("html");
+const bodyElement = document.querySelector("body");
 
 htmlElement.style.backgroundColor = "black";
+
+bodyElement.style.margin = "0 auto";
+bodyElement.style.height = "100vh";
+bodyElement.style.fontFamily = "KIMM_Bold";
